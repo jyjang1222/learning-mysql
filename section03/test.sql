6dba3315114ee7e66b4aaca5e860414d35f67259
JJY <jyjang1222@naver.com>
2022-11-29 11:27:05 +0900

    section3 update

---
+++ section03/test.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE hello_world_db;
+
+SHOW DATABASES;
+
+DROP DATABASE hello_world_db;
+
+CREATE DATABASE dog_walking_app;
+USE dog_walking_app;
+SELECT database();
+
+DROP DATABASE dog_walking_app;
